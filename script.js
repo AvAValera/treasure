9c572a610ecc41e86513c1281fd6152a755e7053
AvAValera <avavarenik07@gmail.com>
2021-02-07 19:07:05 +0200

    create marker navigation

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ const target ={
     y : 0,
 };
 mapZone.addEventListener('click',(e) =>{
-    const distance = (e.offsetX + target.x) - (e.offsetY + target.y)
+    const marker = document.querySelector('.marker')
+    marker.style.display = 'block';
+    const distance = (e.offsetX - target.x) - (e.offsetY - target.y)
     console.log(distance);
     if(distance <= 15 && distance >= -15){
         console.log('WIN');
     }
+
+    marker.style.left = (e.offsetX - 10)+ 'px' ;
+    marker.style.top = (e.offsetY - 10)+ 'px';
+
 })
 function startGame() {
     const countMove = document.querySelector('.move__num')
     countMove.textContent = 0;
     target.x = Math.floor(Math.random() * 590);
     target.y = Math.floor(Math.random() * 520);
-
 }
 startGame();
 console.log(target.x + ' ' + target.y);
