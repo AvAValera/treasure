5d6c8abe220eb649f39a794995074b261c55926a
AvAValera <avavarenik07@gmail.com>
2021-02-08 22:19:13 +0200

    add images, create game logic

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ const mapZone = document.querySelector('.map__zone');
 const btnStart = document.querySelector('.btn_start')
 const countMove = document.querySelector('.move__num')
 const zoneMarker = document.querySelector('.zone_marker')
+const gameZone = document.querySelector('.map__zone')
 const target ={
     x : 0,
     y : 0,
 };
+let count;
+let winner = false;
+
 mapZone.addEventListener('click',(e) =>{
-    const marker = document.createElement('div');
-    marker.style.zIndex = 0;
-    const distance = (e.offsetX - target.x) - (e.offsetY - target.y)
-    
-    if(distance <= 15 && distance >= -15){
-        console.log('WIN');
+    if(count >= 1 && !winner){
+        const marker = document.createElement('img');
+        const distanceToTarget = (e.offsetX - target.x) - (e.offsetY - target.y)
+        console.log(distanceToTarget);
+        marker.className = 'marker';
+        zoneMarker.appendChild(marker);
+        marker.src = 'style/../img/shovel.png';
+        marker.style.left = (e.offsetX - 30)+ 'px' ;
+        marker.style.top = (e.offsetY - 50)+ 'px';
+        // console.log(`X ${e.offsetX} Y ${e.offsetY}`);
+        count -= 1;
+        buttonRestart();
+        countMove.textContent = count;
+        if(distanceToTarget <= 15 && distanceToTarget >= -15){
+            marker.src = 'style/../img/treasure.png';
+            console.log('WIN');
+            count = 0;
+            winner = true;
+        }
     }
-    marker.className = 'marker';
-    zoneMarker.appendChild(marker);
-    marker.style.left = (e.offsetX - 10)+ 'px' ;
-    marker.style.top = (e.offsetY - 10)+ 'px';
-    console.log(`X ${e.offsetX} Y ${e.offsetY}`);
-    
-    countMove.textContent -= 1;
-})
+});
+gameZone.onmouseover = () => gameZone.style.opacity = 0.6;
+gameZone.onmouseout = () => gameZone.style.opacity = 0;
 
 btnStart.addEventListener('click',() =>{
     const marker = document.querySelectorAll('.marker');
@@ -36,16 +48,23 @@ btnStart.addEventListener('click',() =>{
 
 
 function startGame() {
-    countMove.textContent = 10;
+    count = 10;
+    buttonRestart();
+    countMove.textContent = count;
     target.x = Math.floor(Math.random() * 590);
     target.y = Math.floor(Math.random() * 520);
     
 };
 
+function buttonRestart(){
+    const btnText = btnStart.querySelector('p')
+    if(count <= 9){
+        btnText.textContent = 'Restart';
+
+    }
+    if (count === 10) btnText.textContent = 'Start';
+};
 
 
 
 
-
-
-console.log(target.x + ' ' + target.y);
