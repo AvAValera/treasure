a2600becbef424c61a1e8daba6ef7d7e85b65990
AvAValera <avavarenik07@gmail.com>
2021-02-08 14:57:08 +0200

    add scss

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ mapZone.addEventListener('click',(e) =>{
 })
 function startGame() {
     const countMove = document.querySelector('.move__num')
-    countMove.textContent = 0;
+    countMove.textContent = 10;
     target.x = Math.floor(Math.random() * 590);
     target.y = Math.floor(Math.random() * 520);
 }
